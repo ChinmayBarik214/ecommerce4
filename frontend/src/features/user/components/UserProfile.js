f0cc5b9b14d6f25767f18bec8d1838bde3a3b5c3
Chinmay <chinmaybarik214@gmail.com>
2024-05-04 14:00:34 +0530

    tut 5:25:40 added add address feature in profile

--- frontend/src/features/user/components/UserProfile.js
+++ frontend/src/features/user/components/UserProfile.js
@@ -7,6 +7,7 @@ export default function UserProfile() {
   const dispatch = useDispatch();
   const user = useSelector(selectUserInfo);
   const [selectedEditIndex, setSelectedEditIndex] = useState(-1);
+  const [showAddAddressForm, setShowAddAddressForm] = useState(false);
   const {
     register,
     handleSubmit,
@@ -27,15 +28,20 @@ export default function UserProfile() {
   };
   const handleEditForm = (index) => {
     setSelectedEditIndex(index);
-    const address = user.addresses[index]
-    setValue('name', address.name)
-    setValue('email', address.email)
-    setValue('city', address.city)
-    setValue('state', address.state)
-    setValue('pinCode', address.pinCode)
-    setValue('street', address.street)
-    setValue('phone', address.phone)
+    const address = user.addresses[index];
+    setValue("name", address.name);
+    setValue("email", address.email);
+    setValue("city", address.city);
+    setValue("state", address.state);
+    setValue("pinCode", address.pinCode);
+    setValue("street", address.street);
+    setValue("phone", address.phone);
   };
+  const handleAdd = (address) => {
+    const newUser = { ...user, addresses: [...user.addresses, address] };
+    dispatch(updateUserAsync(newUser));
+    setShowAddAddressForm(false);
+  }
   return (
     <div>
       <div className="mx-auto mt-12 bg-white max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -49,6 +55,179 @@ export default function UserProfile() {
         </div>
 
         <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+          <button
+            onClick={e=>{setShowAddAddressForm(true);setSelectedEditIndex(-1)}}
+            type="submit"
+            className="rounded-md my-5 bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Add New Address
+          </button>
+          {showAddAddressForm ? (
+                <form
+                  className="bg-white px-5 py-12 mt-12"
+                  noValidate
+                  onSubmit={handleSubmit((data) => {
+                    console.log(data);
+                    handleAdd(data);
+                    reset();
+                  })}
+                >
+                  <div className="space-y-12">
+                    <div className="border-b border-gray-900/10 pb-12">
+                      <h2 className="text-2xl font-semibold leading-7 text-gray-900">
+                        Personal Information
+                      </h2>
+                      <p className="mt-1 text-sm leading-6 text-gray-600">
+                        Use a permanent address where you can receive mail.
+                      </p>
+
+                      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                        <div className="sm:col-span-4">
+                          <label
+                            htmlFor="name"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Full name
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              type="text"
+                              {...register("name", {
+                                required: "name is required",
+                              })}
+                              id="name"
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+
+                        <div className="sm:col-span-4">
+                          <label
+                            htmlFor="email"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Email address
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              id="email"
+                              {...register("email", {
+                                required: "email is required",
+                              })}
+                              type="email"
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+
+                        <div className="sm:col-span-3">
+                          <label
+                            htmlFor="phone"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Phone
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              id="phone"
+                              {...register("phone", {
+                                required: "phone number is required",
+                              })}
+                              type="tel"
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+
+                        <div className="col-span-full">
+                          <label
+                            htmlFor="street-address"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Street address
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              type="text"
+                              {...register("street", {
+                                required: "street address is required",
+                              })}
+                              id="street"
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+
+                        <div className="sm:col-span-2 sm:col-start-1">
+                          <label
+                            htmlFor="city"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            City
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              type="text"
+                              {...register("city", {
+                                required: "city is required",
+                              })}
+                              id="city"
+                              autoComplete="address-level2"
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+
+                        <div className="sm:col-span-2">
+                          <label
+                            htmlFor="region"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            State / Province
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              type="text"
+                              {...register("state", {
+                                required: "state is required",
+                              })}
+                              id="state"
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+
+                        <div className="sm:col-span-2">
+                          <label
+                            htmlFor="pinCode"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            ZIP / Postal code
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              type="text"
+                              {...register("pinCode", {
+                                required: "pincode is required",
+                              })}
+                              id="pinCode"
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="mt-6 flex items-center justify-end gap-x-6">
+                      <button
+                        type="submit"
+                        className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      >
+                        Add Address
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              ) : null}
           <p className="mt-0.5 text-sm text-gray-500">Your Addresses :</p>
           {user.addresses.map((address, index) => (
             <div>
@@ -208,8 +387,8 @@ export default function UserProfile() {
                       </div>
                     </div>
                     <div className="mt-6 flex items-center justify-end gap-x-6">
-                    <button
-                        onClick={e => setSelectedEditIndex(-1)}
+                      <button
+                        onClick={(e) => setSelectedEditIndex(-1)}
                         type="submit"
                         className="rounded-md px-3 py-2 text-sm font-semibold text-gray shadow-sm hover:bg-gray-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                       >
