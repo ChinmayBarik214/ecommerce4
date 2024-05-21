4d9a890e91fc06025cb55185b5aed301f6a2064b
Chinmay <chinmaybarik214@gmail.com>
2024-05-21 19:52:08 +0530

    tut 9:07:35 frontend pt 21 complete

--- frontend/src/features/user/components/UserOrders.js
+++ frontend/src/features/user/components/UserOrders.js
@@ -9,12 +9,12 @@ import { discountedPrice } from '../../../app/constants';
 
 export default function UserOrders() {
   const dispatch = useDispatch();
-  const user = useSelector(selectUserInfo);
+  const userInfo = useSelector(selectUserInfo);
   const orders = useSelector(selectUserOrders);
 
   useEffect(() => {
-    dispatch(fetchLoggedInUserOrderAsync(user?.id)); // test before removing the '?'
-  }, [dispatch, user]);
+    dispatch(fetchLoggedInUserOrderAsync(userInfo?.id)); // test before removing the '?'
+  }, [dispatch, userInfo]);
 
   return (
     <div>
--- frontend/src/pages/Checkout.js
+++ frontend/src/pages/Checkout.js
@@ -8,7 +8,7 @@ import {
 } from "../features/cart/cartSlice";
 import { Navigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { updateUserAsync } from "../features/auth/authSlice";
+import { updateUserAsync } from "../features/user/userSlice";
 import {
   createOrderAsync,
   selectCurrentOrder,
@@ -373,9 +373,13 @@ function Checkout() {
                           <div>
                             <div className="flex justify-between text-base font-medium text-gray-900">
                               <h3>
-                                <a href={item.product.id}>{item.product.title}</a>
+                                <a href={item.product.id}>
+                                  {item.product.title}
+                                </a>
                               </h3>
-                              <p className="ml-4">${discountedPrice(item.product)}</p>
+                              <p className="ml-4">
+                                ${discountedPrice(item.product)}
+                              </p>
                             </div>
                             <p className="mt-1 text-sm text-gray-500">
                               {item.product.brand}
